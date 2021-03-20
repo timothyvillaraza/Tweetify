2796cfe49454d1da19853c74fed06e0037571d1b
danhrubec <60075606+danhrubec@users.noreply.github.com>
2021-03-20 13:06:35 -0500

    spotify creating a playlist

--- spotify.py
+++ spotify.py
@@ -1,10 +1,19 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 import TOKENS
 
 birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
 post_uri = 'spotify:artist:246dkjvS1zLTtiykXe5h60'
+
+
+
+scope = 'playlist-modify-public'
+username = 'daniel_hrubec'
+token=SpotifyOAuth(scope=scope, username=username, client_id=TOKENS.SPOTIPY_CLIENT_ID, client_secret=TOKENS.SPOTIPY_CLIENT_SECRET, redirect_uri=TOKENS.SPOTIPY_REDIRECT_URI)
+
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=TOKENS.SPOTIPY_CLIENT_ID, client_secret=TOKENS.SPOTIPY_CLIENT_SECRET))
+spotifyObjectAuth = spotipy.Spotify(auth_manager=token)
 
 results = spotify.artist_albums(post_uri, album_type='album')
 albums = results['items']
@@ -19,7 +28,7 @@ for album in albums:
 
 lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
 
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=TOKENS.SPOTIPY_CLIENT_ID, client_secret=TOKENS.SPOTIPY_CLIENT_SECRET))
+# spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=TOKENS.SPOTIPY_CLIENT_ID, client_secret=TOKENS.SPOTIPY_CLIENT_SECRET))
 results = spotify.artist_top_tracks(lz_uri)
 
 for track in results['tracks'][:10]:
@@ -27,3 +36,14 @@ for track in results['tracks'][:10]:
     print('audio    : ' + track['preview_url'])
     print('cover art: ' + track['album']['images'][0]['url'])
     print()
+
+
+#creating a playlist section
+
+playlist_name = 'twitter spotify playlist test'
+playlist_description = 'bigNUT'
+spotifyObjectAuth.user_playlist_create(user=username,name=playlist_name,public=True,description=playlist_description)
+
+
+
+
