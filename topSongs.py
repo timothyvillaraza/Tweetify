80a42b0146025507676418cedea3eb1a1cd22f3c
dpatel9190 <deveshboss02@gmail.com>
2021-03-20 16:05:14 -0500

    topSongs.py updated

--- topSongs.py
+++ topSongs.py
@@ -1,14 +1,26 @@
 import billboard
 import re
+
 chart = billboard.ChartData('hot-100')
 top_25_titles = []
 top_25_artists = []
 i = 0;
 while i < 25:
-    entry = re.sub(r'\W+','',chart[i].title).upper()
-    top_25_titles.append(entry)
     entry = re.sub(r'\W+','',chart[i].artist).upper()
-    top_25_artists.append(entry)
-    print(entry)
+    featureCount = entry.find("FEATURING")
+    if featureCount != -1:
+        print(entry[:featureCount])
+        top_25_titles.append(entry[:featureCount])
+        featureCount += 9
+        print(entry[featureCount:])
+        top_25_titles.append(entry[featureCount:])
+        i+=2
+    else:
+        print(entry)
+        top_25_titles.append(entry)
+        i+=1
+    #top_25_titles.append(entry)
+    #entry = re.sub(r'\W+','',chart[i].artist).upper()
+    #top_25_artists.append(entry)
+    #print(entry)
     #print(chart[i].artist)
-    i+=1
