9c24a59d01ed31a06fdd71b1e9a6007b95573af6
dpatel9190 <deveshboss02@gmail.com>
2021-03-20 12:31:44 -0500

    audio

--- spotify.py
+++ spotify.py
@@ -2,8 +2,6 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import TOKENS
 
-
-
 birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
 post_uri = 'spotify:artist:246dkjvS1zLTtiykXe5h60'
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=TOKENS.SPOTIPY_CLIENT_ID, client_secret=TOKENS.SPOTIPY_CLIENT_SECRET))
@@ -16,3 +14,16 @@ while results['next']:
 
 for album in albums:
     print(album['name'])
+
+
+
+lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=TOKENS.SPOTIPY_CLIENT_ID, client_secret=TOKENS.SPOTIPY_CLIENT_SECRET))
+results = spotify.artist_top_tracks(lz_uri)
+
+for track in results['tracks'][:10]:
+    print('track    : ' + track['name'])
+    print('audio    : ' + track['preview_url'])
+    print('cover art: ' + track['album']['images'][0]['url'])
+    print()
